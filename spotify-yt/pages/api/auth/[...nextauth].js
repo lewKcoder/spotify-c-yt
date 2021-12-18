2a157650b74cbfdc783cebf0ba0d4f7df2880af7
Yu <7kamimura74@gmail.com>
2021-12-19 01:04:27 +0900

    add:next-auth骨組み追加

--- spotify-yt/pages/api/auth/[...nextauth].js
+++ spotify-yt/pages/api/auth/[...nextauth].js
@@ -1,33 +1,33 @@
-import NextAuth from "next-auth"
-import SpotifyProviders from "next-auth/providers/spotify"
-import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
 
-async function refreshAccessToken() {
+async function refreshAccessToken(token) {
   try {
     spotifyApi.setAccessToken(token.accessToken);
     spotifyApi.setAccessToken(token.refreshToken);
 
-    const { body: refreshAccessToken } = await spotifyApi.refreshAccessToken();
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
 
     return {
       ...token,
-      accessToken: refreshAccessToken.access_token,
-      accessTokenExpires: Date.now + refreshToken.expires_in * 1000
-    }
-
-  } catch(error) {
-    console.error(error)
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.access_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error(error);
     return {
       ...token,
-      error: "RefreshTokenError"
-    }
+      error: "RefreshTokenError",
+    };
   }
 }
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    SpotifyProviders({
+    SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
       authorization: LOGIN_URL,
@@ -39,21 +39,29 @@ export default NextAuth({
     signIn: "/login",
   },
   callbacks: {
-    async jwt({token, account, user}){
-      if(account && user) {
+    async jwt({ token, account, user }) {
+      if (account && user) {
         return {
           ...token,
           accessToken: account.access_token,
           refreshToken: account.refresh_token,
           username: account.providerAccountId,
           accessTokenExpires: account.expires_at * 1000,
-        }
+        };
       }
-      if(Date.now() < token.accessTokenExpires){
+      if (Date.now() < token.accessTokenExpires) {
         return token;
       }
 
-      return await refreshAccessToken(token)
-    }
-  }
-})
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
+  },
+});
