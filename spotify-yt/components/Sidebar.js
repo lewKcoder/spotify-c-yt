219d58a4b0b20b40c923c7e13afcc3715a1d8b73
Yu <7kamimura74@gmail.com>
2021-12-22 15:00:50 +0900

    Add: Center.jsComponents

--- spotify-yt/components/Sidebar.js
+++ spotify-yt/components/Sidebar.js
@@ -1,34 +1,47 @@
-import React from 'react'
-import {HomeIcon, SearchIcon, LibraryIcon, HeartIcon, RssIcon, PlusCircleIcon} from '@heroicons/react/outline'
+import React from "react";
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  HeartIcon,
+  RssIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/outline";
+import { signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll npm install scrollbar-hide h-screen">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5"/>
-          <p>Home</p>
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <HomeIcon className="h-5 w-5" />
+          <p>Log out</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="h-5 w-5"/>
+          <SearchIcon className="h-5 w-5" />
           <p>Search</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5"/>
+          <LibraryIcon className="h-5 w-5" />
           <p>Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
 
         <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5"/>
+          <PlusCircleIcon className="h-5 w-5" />
           <p>Create PlayList</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5"/>
+          <HeartIcon className="h-5 w-5" />
           <p>Like Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5"/>
+          <RssIcon className="h-5 w-5" />
           <p>Your episode</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
@@ -45,7 +58,7 @@ function Sidebar() {
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
